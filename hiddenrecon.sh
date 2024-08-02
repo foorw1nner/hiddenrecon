52655323e8f75b287d05b278a8eb6f433cfaa086
foorw1nner <108235281+foorw1nner@users.noreply.github.com>
2024-08-02 19:03:22 -0400

    Update hiddenrecon.sh

--- hiddenrecon.sh
+++ hiddenrecon.sh
@@ -53,7 +53,7 @@ then
 
 	for url in "${list[@]}"
 	do
-		parameters=$(curl -Lsk "$url" | grep -Eo $'data\-[A-Za-z0-9_-]+=\'\'|data\-[A-Za-z0-9_-]+=""|<input[^>]*type=\'hidden\'[^>]*value=[^>]*>|<input[^>]*type="hidden"[^>]*value=[^>]*>' | tr -s ' ' '\n' | grep -Eo $'^data\-[A-Za-z0-9_-]+=\'\'|^data\-[A-Za-z0-9_-]+=""|^name=\'[A-Za-z0-9_-]+\'|^name="[A-Za-z0-9_-]+"' | sed -E s'/name=|data\-//' | tr -d $'\'"' | awk -F '=' '{print $1"=hiddenrecon"}' | sort -u | tr -s '\n' '&'  | sed s'/\&$//')	
+		parameters=$(curl -Lsk "$url" | grep -Eo $'data\-[A-Za-z0-9_-]+=\'\'|data\-[A-Za-z0-9_-]+=""|<input[^>]*type=\'hidden\'[^>]*value=[^>]*>|<input[^>]*type="hidden"[^>]*value=[^>]*>' | tr -s ' ' '\n' | grep -Eo $'^data\-[A-Za-z0-9_-]+=\'\'|^data\-[A-Za-z0-9_-]+=""|^name=\'[^\']*\'|^name="[^"]*"' | sed -E s'/name=|data\-//' | tr -d $'\'"' | awk -F '=' '{print $1"=hiddenrecon"}' | sort -u | tr -s '\n' '&'  | sed s'/\&$//')	
 		
 		assembling_url
 	done
@@ -70,7 +70,7 @@ then
 
 		for url in "${list[@]}"
 		do
-			parameters=$(curl -Lsk "$url" | grep -Eo $'<input[^>]*type=\'hidden\'[^>]*value=[^>]*>|<input[^>]*type="hidden"[^>]*value=[^>]*>' | tr -s ' ' '\n' | grep -Eo $'^name=\'[A-Za-z0-9_-]+\'|^name=\"[A-Za-z0-9_-]+\"' | cut -d '=' -f2 | tr -d $'\'"' | sed s'/$/=hiddenrecon/' | sort -u | tr -s '\n' '&' | sed s'/\&$//')
+			parameters=$(curl -Lsk "$url" | grep -Eo $'<input[^>]*type=\'hidden\'[^>]*value=[^>]*>|<input[^>]*type="hidden"[^>]*value=[^>]*>' | tr -s ' ' '\n' | grep -Eo $'^name=\'[^\']*\'|name="[^"]*"' | cut -d '=' -f2 | tr -d $'\'"' | sed s'/$/=hiddenrecon/' | sort -u | tr -s '\n' '&' | sed s'/\&$//')
 		
 			assembling_url
 		done
